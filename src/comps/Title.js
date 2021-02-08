00784251c05454f4161ff7572b45f23fe65cabbd
LishuGupta652 <lishugupta652@gmail.com>
2021-02-08 06:53:32 +0000

    final changes

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
       <h2>Upload Your Picture</h2>
     </div>
-  )
-}
+  );
+};
 
 export default Title;
--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,35 +1,34 @@
-import React, {useState} from 'react'
-import ProgressBar from './ProgressBar'
+import React, { useState } from "react";
+import ProgressBar from "./ProgressBar";
 
 function UploadForm() {
-    const [file, setFile] = useState(null)
-    const [error, setError] = useState(null)
-    const types = ['image/png', 'image/jpeg', 'image/jpg']
-    const handleChange = (e) => {
-        let selected = e.target.files[0];
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
+  const types = ["image/png", "image/jpeg", "image/jpg"];
+  const handleChange = (e) => {
+    let selected = e.target.files[0];
 
-        if(selected && types.includes(selected.type)) {
-            setFile(selected);
-        } else {
-            setFile(null);
-            setError("Please select an image file. (png or jpeg)")
-        }
+    if (selected && types.includes(selected.type)) {
+      setFile(selected);
+    } else {
+      setFile(null);
+      setError("Please select an image file. (png or jpeg)");
     }
-    return (
-        <form>
-           
-            <label>
-                <input type="file" onChange={handleChange} />
-                <span>+</span>
-            </label>
+  };
+  return (
+    <form>
+      <label>
+        <input type="file" onChange={handleChange} />
+        <span>+</span>
+      </label>
 
-            <div className="output">
-                {error && <div className="error">{error}</div>}
-                {file && <div>{file.name} </div>}
-                { file &&  <ProgressBar file={file} setFile={setFile}/>}
-            </div>
-        </form>
-    )
+      <div className="output">
+        {error && <div className="error">{error}</div>}
+        {file && <div>{file.name} </div>}
+        {file && <ProgressBar file={file} setFile={setFile} />}
+      </div>
+    </form>
+  );
 }
 
 export default UploadForm;
